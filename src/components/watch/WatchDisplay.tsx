6a210302ec5f83b34e2ea53c251107d507da03f0
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-09-16 00:41:30 +0100

    v1 of watch face design

--- src/components/watch/WatchDisplay.tsx
+++ src/components/watch/WatchDisplay.tsx
@@ -1,5 +1,5 @@
 import {ChartBarIcon, StarIcon} from "@heroicons/react/24/outline";
-import GitHubIcon from "../../assets/img/github.svg";
+import {useState} from "react";
 
 interface tempProps {
     title: string;
@@ -10,20 +10,33 @@ interface tempProps {
     gitstars: string;
 }
 
-function WatchDisplay({title, ratings, img, url, giturl, gitstars}: tempProps) {
+function WatchDisplay({title, ratings, img, url, gitstars}: tempProps) {
+
+    const [display, setDisplay] = useState(false);
 
     function openPage() {
         window.open(url);
     }
 
+    function setHoverStart() {
+        setDisplay(true);
+    }
+
+    function setHoverEnd() {
+        setDisplay(false);
+    }
+
+
     return (
-        <div className="card cursor-pointer overflow-hidden grid grid-cols-2" onClick={openPage}>
+        <div className="card cursor-pointer overflow-hidden grid grid-cols-2" onClick={openPage}
+             onMouseEnter={setHoverStart} onMouseLeave={setHoverEnd}>
 
             <div className="flex w-full h-full justify-center items-center">
                 <img className="w-40" src={img}/>
             </div>
 
-            <div className="grid [grid-template-areas:'stack'] bg-black text-white">
+            <div
+                className={"grid [grid-template-areas:'stack'] text-white " + (display ? "bg-gray-800" : "bg-gray-900")}>
 
                 <div className="flex justify-center items-center [grid-area:stack]">
                     <h1 className="w-40 text-2xl font-bold">{title}</h1>
@@ -32,13 +45,16 @@ function WatchDisplay({title, ratings, img, url, giturl, gitstars}: tempProps) {
                 <div className="flex items-end w-full h-full [grid-area:stack] pb-1">
 
                     <div className="grid grid-cols-2 w-full px-2 items-center">
+
+                        {/*
                         <div className="">
                             {giturl && (
-                                <img className="w-10 text-red-500" src={GitHubIcon}/>
+                                <img className="w-10 text-red-500" src={GitHubIcon} onClick={openGitHub}/>
                             )}
                         </div>
+                        */}
 
-                        <div className="flex items-end justify-end gap-2">
+                        <div className="col-start-2 flex items-end justify-end gap-2">
 
                             {gitstars && (
                                 <div>
