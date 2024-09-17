f40303096ffa7399a6ee011c2df02e749fd51503
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-09-18 00:00:25 +0100

    Moved videos to json file

--- src/components/view/VidView.tsx
+++ src/components/view/VidView.tsx
@@ -1,4 +1,5 @@
 import Wave from "../util/Wave.tsx";
+import Videos from "../../assets/data/videos.json";
 
 function VidView() {
 
@@ -9,35 +10,14 @@ function VidView() {
 
                 <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4 px-2 py-2">
 
-                    <div className="card overflow-hidden">
-                        <iframe width="380" height="280" src="https://www.youtube.com/embed/XeEITqNPDFY?si=LAt9RSVBpX-S5Rqr"
-                                title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    </div>
-
-                    <div className="card overflow-hidden">
-                        <iframe width="380" height="280" src="https://www.youtube.com/embed/Qxiqgw56k1o?si=lH8p9R5QldGC87YN"
-                                title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    </div>
-
-                    <div className="card overflow-hidden">
-                        <iframe width="380" height="280"
-                                src="https://www.youtube.com/embed/r7RtaeCx-7o?si=b09dnhym4MWfIgks"
-                                title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    </div>
-
-                    <div className="card overflow-hidden">
-                        <iframe width="380" height="280"
-                                src="https://www.youtube.com/embed/eJxtoCWF4Sg?si=o4b4KcgJeUNxxDWW"
-                                title="YouTube video player" frameBorder="0"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-                    </div>
+                    {Videos.map((video, index) => (
+                        <div className="card overflow-hidden" key={index}>
+                            <iframe width="380" height="280" src={video}
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                    referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+                        </div>
+                    ))}
 
                 </div>
 
