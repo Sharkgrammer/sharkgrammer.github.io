1696bb75747fe3d74c2bd7c91c76b9abcdb0c946
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-09-24 16:43:35 +0100

    Added GitHub stats image

---
+++ src/components/view/ProjectView.tsx
@@ -0,0 +1,26 @@
+function ProjectView() {
+
+    return (
+        <div className="py-5">
+
+            <div className="flex justify-center w-full px-2 text-center md:text-left">
+
+                <div className="md:w-2/3 flex flex-col gap-2">
+                    <h1 className="text-3xl font-semibold">Personal GitHub Stats</h1>
+
+                    <div className="text-left">
+                        <p>I wrote a lightweight GitHub stats script for displaying information about my account.</p>
+                        <p>It just uses Python and GitHub actions to run and keep the below image up to date with my
+                            actions on GitHub.</p>
+                    </div>
+
+                    <img className="" src="https://sharkgrammer.github.io/Lightweight-GitHub-Stats/data.png"/>
+                </div>
+            </div>
+
+        </div>
+    )
+
+}
+
+export default ProjectView
