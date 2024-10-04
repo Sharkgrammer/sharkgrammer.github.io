387024cdd207361dd3929721bef02114b54a5aa0
Daniel Keane Kelly <danielkeanekelly@gmail.com>
2024-10-04 21:36:59 +0100

    Version 1 of new build

--- README.md
+++
@@ -1,2 +0,0 @@
-# GitHub site
-Just a repo for hosting the personal site for my repos, [link here](https://sharkgrammer.github.io/).
--- src/components/project/Cell.tsx
+++
@@ -1,8 +0,0 @@
-function Cell() {
-
-    return (
-        <div className="cell bg-white border border-3-shadow"/>
-    )
-}
-
-export default Cell
